80ceac2ce7d96b802797180fe69508c0328d5098
franzparks <franzparks@gmail.com>
2016-04-27 13:36:27 -0700

    chore: update port

--- application.py
+++ application.py
@@ -486,5 +486,4 @@ if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = False
     port = int(os.environ.get("PORT", 33507))
-    app.run(host='0.0.0.0', port)
-    
+    app.run(host='0.0.0.0', port=port)
